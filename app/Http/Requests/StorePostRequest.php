8aafcffe798f049cc755298ccf5b04c34a3b54d9
Subigya Pokharel <subigyapok08@gmail.com>
2026-02-27 00:28:07 +0545

    Initial project upload

---
+++ app/Http/Requests/StorePostRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+use Illuminate\Http\Request;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules():array
+    {
+        return [
+            'role'=>'required',
+            'name'=>'required|string|min:4|max:30',
+            'email'=>'required|email|unique:users',
+            'password'=>[
+                'required',
+                'confirmed',
+            //     Password::min(8)
+            //         ->letters()      // Requires at least one letter
+            //         ->mixedCase()    // Requires at least one uppercase and one lowercase letter
+            //         ->numbers()      // Requires at least one number
+            //         ->symbols()      // Requires at least one symbol (!, $, #, %)
+            //         ->uncompromised(), // Checks against the Have I Been Pwned database
+            ],
+        ];
+    }
+}
+
