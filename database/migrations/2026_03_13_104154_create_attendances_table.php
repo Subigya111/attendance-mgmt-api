80fbaea248447cdc348830c19f6012bfa0eb8da3
Subigya Pokharel <subigyapok08@gmail.com>
2026-03-14 23:33:56 +0545

    ..

--- database/migrations/2026_03_13_104154_create_attendances_table.php
+++ database/migrations/2026_03_13_104154_create_attendances_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('attendances', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->class_id();
+            $table->student_id();
+            $table->scanned_at();
         });
     }
 
