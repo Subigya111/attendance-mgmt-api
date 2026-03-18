ef725fc6a37ddf0b0b757a57c85ff69317051f28
Subigya Pokharel <subigyapok08@gmail.com>
2026-03-18 23:13:31 +0545

    ....

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Attendance extends Model
 {
-    //
+    public function student():BelongsTo{
+        return $this->belongsTo(StartClass::class,'class_id');
+    }
 }
--- app/Models/StartClass.php
+++ app/Models/StartClass.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class StartClass extends Model
 {
     public function teacher():BelongsTo{
         return $this->belongsTo(User::class,'teacher_id');
+    }
+    public function attendance():HasMany{
+        return $this->hasMany(Attendance::class,'class_id');
     }
 }
