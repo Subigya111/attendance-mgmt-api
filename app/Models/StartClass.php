10c74b13a6a312d70209830fe2b45b72fdebc09e
Subigya Pokharel <subigyapok08@gmail.com>
2026-03-18 22:53:11 +0545

    ...

--- app/Models/StartClass.php
+++ app/Models/StartClass.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class StartClass extends Model
 {
-    //
+    public function teacher():BelongsTo{
+        return $this->belongsTo(User::class,'teacher_id');
+    }
 }
