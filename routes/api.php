0154fe517b69b86de6dcab237ea53c8f9654031c
Subigya Pokharel <subigyapok08@gmail.com>
2026-03-19 13:38:17 +0545

    ......

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,14 +18,14 @@ Route::post('/logout',[AuthenticationController::class,'logout'])->middleware('a
 
 //routes for teacher
 Route::middleware(['auth:sanctum','role:teacher'])->group(function(){
-    Route::post('/class/create');
-    Route::get('/class/{id}/attendance');
+    Route::post('/class/create',[TeacherController::class,'generateQr']);
+    Route::get('/class/{id}/attendance',[TeacherController::class,'showAllAttendance']);
 });
 
 //routes for student
 Route::middleware(['auth:sanctum','role:student'])->group(function(){
-    Route::post('/attendance/mark');
-    Route::get('/attendance/my');
+    Route::post('/attendance/mark',[StudentController::class,'markAttendance']);
+    Route::get('/attendance/my',[StudentController::class,'showMyAttendance']);
 });
 
 
