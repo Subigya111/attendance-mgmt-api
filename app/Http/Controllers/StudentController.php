4a940f125bf346883050e26bd75b0b47da488c34
Subigya Pokharel <subigyapok08@gmail.com>
2026-04-21 22:30:24 +0545

    ..

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -9,6 +9,7 @@ use App\Models\StartClass;
 class StudentController extends Controller
 {
     public function markAttendance(){
+
         $token=request()->classtoken;
         $start_class = StartClass::where('classToken', $token)->first();
         if (!$start_class){
@@ -16,10 +17,26 @@ class StudentController extends Controller
                 'message'=>'Could not find the class'
             ],404);
         }
+        $studentId=auth()->id();
+        $classId=$start_class->id;
+        $exists=Attendance::where('student_id',$studentId)
+                            ->where('class_id',$classId)
+                            ->exists();
+        if ($exists){
+            return response()->json([
+                'message'=>'Attendance already marked'
+            ],400);
+        }
         Attendance::create([
-            'class_id'=>$start_class->id(),
-            'student_id'=>auth()->id()
+            'class_id'=>$classId,
+            'student_id'=>$studentId
+            
         ]);
+        return response()->json([
+                'message'=>'Successfull!!'
+        ],200);
+
+
     }
     public function showMyAttendance(){
         $studentId=auth()->id();
